24612f95e76d16d7d30e431d6584ec99c9d36056
GiggleLink <linhvu0395@gmail.com>
2023-02-24 22:04:28 +0700

    fixed hashrouter

--- src/component/Nav/Nav.js
+++ src/component/Nav/Nav.js
@@ -4,16 +4,16 @@ function Nav() {
   return (
     <div className="nav-box">
       <div className="nav">
-        <a href="/" className="nav-link">
+        <a href="/#/" className="nav-link">
           About
         </a>
-        <a href="/project" className="nav-link">
+        <a href="/#/project" className="nav-link">
           Project
         </a>
-        <a href="/experience" className="nav-link">
+        <a href="/#/experience" className="nav-link">
           Experience
         </a>
-        <a href="/contact" className="nav-link">
+        <a href="/#/contact" className="nav-link">
           Contact
         </a>
       </div>
