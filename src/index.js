d64d841ea3b65cc33bfa92c7409262c7411917cf
GiggleLink <linhvu0395@gmail.com>
2023-02-24 22:15:16 +0700

    hashrouter as router

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { HashRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter hashType="slash">
+  <Router hashType="slash">
     <App />
-  </HashRouter>
+  </Router>
 );
